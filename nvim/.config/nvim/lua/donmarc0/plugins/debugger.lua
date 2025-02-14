0a2b06b1f32179eff949c3931ba7d289f43d27b3
Marc Novak <novakm@gtue.de>
2025-02-14 13:00:57 +0100

    feat: added node debugging

--- nvim/.config/nvim/lua/donmarc0/plugins/debugger.lua
+++ nvim/.config/nvim/lua/donmarc0/plugins/debugger.lua
@@ -44,6 +44,53 @@ return {
 			keymap.set("n", "<F7>", dap.step_into, { desc = "Step Into" })
 			keymap.set("n", "<F9>", dap.step_out, { desc = "Step Out" })
 
+			dap.adapters["pwa-node"] = {
+				type = "server",
+				host = "localhost",
+				port = "${port}",
+				executable = {
+					command = "node",
+					args = { "~/dev/microsoft/js-debug/src/dapDebugServer.js", "${port}" },
+				},
+			}
+
+			for _, language in ipairs({ "typescript", "javascript" }) do
+				dap.configurations[language] = {
+					{
+						type = "pwa-node",
+						request = "launch",
+						name = "Launch file",
+						program = "${file}",
+						cwd = "${workspaceFolder}",
+					},
+				}
+			end
+
+			-- dap.adapters.node2 = {
+			-- 	type = "executable",
+			-- 	command = "node",
+			-- 	args = { os.getenv("HOME") .. "/dev/microsoft/vscode-node-debug2/out/src/nodeDebug.js" },
+			-- }
+			-- dap.configurations.javascript = {
+			-- 	{
+			-- 		name = "Launch",
+			-- 		type = "node2",
+			-- 		request = "launch",
+			-- 		program = "${file}",
+			-- 		cwd = vim.fn.getcwd(),
+			-- 		sourceMaps = true,
+			-- 		protocol = "inspector",
+			-- 		console = "integratedTerminal",
+			-- 	},
+			-- 	{
+			-- 		-- For this to work you need to make sure the node process is started with the `--inspect` flag.
+			-- 		name = "Attach to process",
+			-- 		type = "node2",
+			-- 		request = "attach",
+			-- 		processId = require("dap.utils").pick_process,
+			-- 	},
+			-- }
+
 			--[[ -- Adapters
 			dap.adapters.coreclr = {
 				type = "executable",
